de83b4fa8fdd4ffe341ab9df03b39225d47b6b32
jpapejr <jpapejr@icloud.com>
2019-12-03 11:08:23 -0500

    Update oc fetch location
    Change oc client fetch location and file handling to be simpler/more efficient

--- bin/pull.sh
+++ bin/pull.sh
@@ -138,20 +138,19 @@ fi
 TOOL=oc_3_11
 if [ "$OS" == "linux" ]; then
         echo "Fetching $TOOL for $OS"
-        wget -q  https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz -O $TOOLBOX_HOME/$OS/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+        wget -q  oc.tar.gz -O $TOOLBOX_HOME/$OS/oc.tar.gz
         cd $TOOLBOX_HOME/$OS
-        tar xfz openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc 
-        mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc oc-3.11
-        rm -fr openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit
-        rm -f openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz 
+        tar xfz $TOOLBOX_HOME/$OS/oc.tar.gz
+        mv oc oc-3.11.157
+        rm -fr $TOOLBOX_HOME/$OS/oc.tar.gz
         cd - > /dev/null
 elif [ "$OS" == "mac" ]; then
         echo "Fetching $TOOL for $OS"
-        wget -q  https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-mac.zip -O $TOOLBOX_HOME/$OS/openshift-origin-client-tools-v3.11.0-0cbc58b-mac.zip
+        wget -q  https://mirror.openshift.com/pub/openshift-v3/clients/3.11.157/macosx/oc.tar.gz -O $TOOLBOX_HOME/$OS/oc.tar.gz
         cd $TOOLBOX_HOME/$OS    
-        unzip -q -d . openshift-origin-client-tools-v3.11.0-0cbc58b-mac.zip oc
-        mv oc oc-3.11
-        rm -fr openshift-origin-client-tools-v3.11.0-0cbc58b-mac.zip
+        tar xvfz $TOOLBOX_HOME/$OS/oc.tar.gz
+        mv oc oc-3.11.157
+        rm -fr $TOOLBOX_HOME/$OS/oc.tar.gz
         cd - > /dev/null
 fi
 
